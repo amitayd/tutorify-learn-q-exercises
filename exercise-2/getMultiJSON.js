bff4c9ce923ba24272112d90d535bd970e7ee8dc
Amitay Dobo <amitayd@gmail.com>
2014-05-05 04:49:11 -0700

    Changed Multiple

---
+++ exercise-2/getMultiJSON.js
@@ -0,0 +1,2 @@
+// It's your job to implement these.
+// Move by creating a test, and then implementing the functionality
---
+++ exercise-2/getMultiJSON.spec.js
@@ -0,0 +1,19 @@
+
+describe('getMultiJSON', function() {
+    it('should be defined', function() {
+        throw new Error('not implemented');
+    });
+    // it('should return a promise');
+    // it('should raise an exception if not passed an array of strings');
+    // describe('when accepting an array of valid urls', function() {
+    //     it('should return a promise');
+    //     it('then() should be called with an array of objects');
+    // });
+    // describe('when accepting an array of invalid urls', function() {
+    //     it('fail() should be called with an error');
+    //     it('error should contained the names of failed urls');
+    // });    
+    
+
+
+});
