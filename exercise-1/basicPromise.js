6e480621f911bbf6406efb833d1a1185a446616c
Amitay Dobo <amitayd@gmail.com>
2014-05-05 03:52:56 -0700

    Changed Multiple

---
+++ exercise-1/basicPromise.js
@@ -0,0 +1,3 @@
+function createBasicPromise(url) {
+    return getJsonPromise(url);
+}
---
+++ exercise-1/basicPromise.spec.js
@@ -0,0 +1,9 @@
+describe ('createBasicPromise()', function() {
+    it('should return an object for a valid url', function() {
+        
+    });
+    
+    it('should raise an exception for invalid url', function() {
+        
+    });
+})
